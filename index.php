7abdb5e0a78ddfd135ef0b84ddb450ac76cf3bae
Florian Hourdin <40579924+naatdev@users.noreply.github.com>
2018-08-01 00:29:33 +0200

    Version 1 release
    Everything's works!

--- index.php
+++ index.php
@@ -18,9 +18,14 @@
 
 Class DB_ {
     private $CONFIG;
+    private $startTime;
+    private $endTime;
+    private $countRequests;
 
-    public function __construct($SETTINGS = array('DIR' => 'DATA')) {
+    public function __construct($SETTINGS = array('DIR' => 'DATA', 'DEBUG' => True, 'TRANSACTION_HISTORY' => True)) {
         $this->CONFIG = $SETTINGS;
+        $this->startTime = microtime(true);
+        $this->countRequests = 0;
     }
 
     public function getPlaceFor($sets = array('key' => '', 'value' => '', 'return' => 'all', 'cache' => True)) {
@@ -71,6 +76,7 @@ Class DB_ {
             }
         }
         
+        $this->countRequests++;
         if($return == 'node') {
             // return the node id
             return $temp_db_block;
@@ -88,6 +94,44 @@ Class DB_ {
         unset($temp_db_block);
     }
 
+    public function newSpawn($sets = array('key' => '', 'value' => '')) {
+        $key = $sets['key'];
+        $value = $sets['value'];
+        $temp_db = new SQLite3('./'.$this->CONFIG['DIR'].'/LIST_ENTRIES.db');
+        $temp_db->busyTimeout(10000);
+        if(empty($temp_db->query('SELECT `PATH` FROM ENTRIES_BY_KEY_VALUE WHERE `KEY` = "'.$key.'" AND `VALUE` = "'.$value.'" ORDER BY ID ASC LIMIT 1')->fetchArray()['PATH'])) {
+            $regdb_db_blocks = new SQLite3("./".$this->CONFIG['DIR']."/NODES.db");
+            $regdb_db_blocks->busyTimeout(10000);
+            $db_block = $regdb_db_blocks->query('SELECT NAME as best FROM LIST_NODES_BY_NAME ORDER BY COUNT_ENTRIES ASC LIMIT 1')->fetchArray()['best'];
+
+            $value_ = str_split($value, 2);
+            $save = '';
+            $context = "/".$db_block;
+
+            foreach ($value_ as $key_ => $value_) {
+                $save .= "/".$value_;
+                if(!is_dir("./".$this->CONFIG['DIR'].$context.$save."/")){
+                    mkdir("./".$this->CONFIG['DIR'].$context.$save."/", 0777);
+                }
+            }
+            if(!file_exists("./".$this->CONFIG['DIR'].$context.$save."/.spawn")) {
+                $regdb_db_blocks->query('UPDATE LIST_NODES_BY_NAME SET COUNT_ENTRIES = COUNT_ENTRIES + 1 WHERE `NAME` = "'.$db_block.'"');
+                $temp_db = new SQLite3('./'.$this->CONFIG['DIR'].'/LIST_ENTRIES.db');
+                $temp_db->busyTimeout(10000);
+                $temp_db_block = $temp_db->query('INSERT INTO ENTRIES_BY_KEY_VALUE(`PATH`,`KEY`,`VALUE`) VALUES("'.$db_block.'","'.$key.'","'.$value.'")');        
+                $fp = fopen("./".$this->CONFIG['DIR'].$context.$save."/.spawn", "a+");
+                if(fputs($fp, "created=".time())) {
+                    $ok = True;
+                }
+                fclose($fp);
+            }
+            $this->countRequests++;
+        }
+        if(isset($ok)) {
+            return True;
+        }
+    }
+
     public function getDataFor($sets = array('key' => '', 'value' => '', 'data' => '')) {
         $key = $sets['key'];
         $value = $sets['value'];
@@ -102,7 +146,7 @@ Class DB_ {
         $context = "./".$this->CONFIG['DIR']."/";
         $context .= $this->getPlaceFor(array('key' => $key, 'value' => $value, 'return' => 'node', 'cache' => True));
         $context .= "/".$save."/".$data;
-
+        $this->countRequests++;
 		if(file_exists($context)){
 			$fp = file_get_contents($context, "r");
 			return $fp;
@@ -112,16 +156,23 @@ Class DB_ {
 		}
     }
     
-    public function setDataFor($sets = array('key' => '', 'value' => '', 'data_key' => '', 'data_value' => '', 'edit' => False)) {
+    public function setDataFor($sets = array('key' => '', 'value' => '', 'data_key' => '', 'data_value' => '')) {
 		$key = $sets['key'];
         $value = $sets['value'];
         $data_key = $sets['data_key'];
         $data_value = $sets['data_value'];
-        $edit = $sets['edit'];
 
 		$value_ = str_split($value, 2);
         $save = '';
         $context = "/".$this->getPlaceFor(array('key' => $key, 'value' => $value, 'return' => 'node', 'cache' => True));
+
+        if(strlen($context) == 1) {
+            $this->newSpawn(array(
+                'key'   => $key,
+                'value' => $value
+            ));
+            $context .= $this->getPlaceFor(array('key' => $key, 'value' => $value, 'return' => 'node', 'cache' => True));
+        }
 
 		foreach ($value_ as $key_ => $value_) {
             $save .= "/".$value_;
@@ -129,21 +180,50 @@ Class DB_ {
 				mkdir("./".$this->CONFIG['DIR'].$context.$save."/", 0777);
 			}
         }
-
-		if(file_exists("./".$this->CONFIG['DIR'].$context.$save."/".$data_key) AND $edit != True){
-			return False;
-		}
-		else{
-			if($edit == True){
-				@unlink("./".$this->CONFIG['DIR'].$context.$save."/".$data_key);
-			}
-			$fp = fopen("./".$this->CONFIG['DIR'].$context.$save."/".$data_key, "a+");
-			if(fputs($fp, $data_value)) {
-                return True;
-            }
-			fclose($fp);
-		}
-	}
+        $this->countRequests++;
+        if(file_exists("./".$this->CONFIG['DIR'].$context.$save."/".$data_key)){
+            @unlink("./".$this->CONFIG['DIR'].$context.$save."/".$data_key);
+        }
+        else{
+            $fp = fopen("./".$this->CONFIG['DIR'].$context.$save."/.history", "a+");
+            fputs($fp, " \n \n --- \n TRANSACTION N° ".uniqid()."  ".time()." == ".date('d-m-Y H:i:s T')." VALUE OF `".$data_key."` IS NOW \n \n ".$data_value." \n \n ---");
+            fclose($fp);
+        }
+        $fp = fopen("./".$this->CONFIG['DIR'].$context.$save."/".$data_key, "a+");
+        if(fputs($fp, $data_value)) {
+            $fp_ = fopen("./".$this->CONFIG['DIR'].$context.$save."/.history", "a+");
+            fputs($fp_, " \n \n --- \n TRANSACTION N° ".uniqid()."  ".time()." == ".date('d-m-Y H:i:s T')." VALUE OF `".$data_key."` IS SET \n \n ".$data_value." \n \n ---");
+            fclose($fp_);
+            $success = True;
+        }
+        fclose($fp);
+        if(isset($success)) {
+            return True;
+        }
+    }
+    
+    public function unSpawn($sets = array('key' => '', 'value' => '')) {
+        $key = $sets['key'];
+        $value = $sets['value'];
+        $temp_db = new SQLite3('./'.$this->CONFIG['DIR'].'/LIST_ENTRIES.db');
+        $temp_db->busyTimeout(10000);
+        $regdb_db_blocks = new SQLite3("./".$this->CONFIG['DIR']."/NODES.db");
+        $regdb_db_blocks->busyTimeout(10000);  
+        $true = 0;
+        $db_block = $this->getPlaceFor(array('key' => $key, 'value' => $value, 'return' => 'node', 'cache' => True));
+        if($regdb_db_blocks->query('UPDATE LIST_NODES_BY_NAME SET COUNT_ENTRIES = COUNT_ENTRIES - 1 WHERE `NAME` = "'.$db_block.'"')) {
+            $true = 1;
+        }
+        if($temp_db->query('DELETE FROM ENTRIES_BY_KEY_VALUE WHERE `KEY` = "'.$key.'" AND `VALUE` = "'.$value.'"')) {
+            $true++;
+        }
+        if($true == 2) {
+            return True;
+        }
+        else{
+            return False;
+        }
+    }
 
     public function version() {
         echo '
@@ -158,6 +238,29 @@ Class DB_ {
                 TrogonCalao DB version 1.0.0
                 Florian Hourdin
                 report bugs here: florian.hourdin59300@gmail.com
+            </pre>
+        ';
+    }
+
+    public function statsDb() {
+        $this->endTime = microtime(true);
+        $time = $this->endTime-$this->startTime;
+        $appreciation = (round($time/$this->countRequests, 5) > 0.00182) ? 'bad' : 'good';
+        echo '
+            <style>pre{border:2px dashed red;padding:50px 10px 50px 10px;}pre::after{position:relative;top:50px;height:10px;color:white;background-color:red;content:"stats db";}</style>
+            <pre>
+                --  8""""8 8""""8 8     8""""8 8"""88    8""""8 8""""8   
+                --  8    " 8    8 8     8    8 8    8    8    8 8    8   
+                --  8e     8eeee8 8e    8eeee8 8    8    8e   8 8eeee8ee 
+                --  88     88   8 88    88   8 8    8    88   8 88     8 
+                --  88   e 88   8 88    88   8 8    8    88   8 88     8 
+                --  88eee8 88   8 88eee 88   8 8eeee8    88eee8 88eeeee8 
+                --   
+                Execution time: '.round($time, 3).'seconds
+                Count requests: '.$this->countRequests.'
+                Avg time per request: '.round($time/$this->countRequests, 5).'seconds
+                How feeling: '.$appreciation.'
+                Date Time: '.date("d-m-Y H:i:s T").'
             </pre>
         ';
     }
--- usage.php
+++ usage.php
@@ -24,24 +24,41 @@ ini_set('display_errors', 1);
 require 'index.php';
 
 $db = new DB_();
-echo $db->getPlaceFor(
-    array(
-        'key'   => 'user',
-        'value' => 'florian',
-        'return'=> 'all',
-        'cache' => True
-    )
-);
+
+/*
+    newSpawn permet de créer un emplacement de stockage pour un futur utilisateur
+    Ceci est facultatif car la fonction est appellée automatiquement lorsque setDataFor est utilisée
+*/
+$db->newSpawn(array(
+    'key'   => 'user', // key est la façon dont cet utilisateur sera identifié au sein de la base
+    'value' => 'flo' // value est son identifiant au sein de la base
+));
+/*
+    setDataFor permet de sauvegarder une donnée pour un utilisateur
+    ou bien de modifier une de ses données déjà existantes
+*/
 $db->setDataFor(array(
-    'key'   => 'user',
-    'value' => 'florian',
-    'data_key'  => 'test',
-    'data_value' => 'hello,world!',
-    'edit' => True
+    'key'   => 'user', // key est la façon dont cet utilisateur sera identifié au sein de la base
+    'value' => 'flo', // value est son identifiant au sein de la base
+    'data_key'  => 'test', // data_key est la façon dont cette donnée sera identifiée au sein de la base
+    'data_value' => 'hello,world!' // data_value est la donnée elle même
 ));
-echo "<br />";
+/*
+    unSpawn permet de supprimer un utilisateur du registre
+    attention ceci ne supprime pas ses données mais juste son enregistrement dans la base
+*/
+$db->unSpawn(array(
+    'key' => 'user', // key est la façon dont cet utilisateur est identifié au sein de la base
+    'value' => 'a' // value est son identifiant au sein de la base
+));
+/*
+    getDataFor permet de récuperer une donnée d'un utilisateur
+    la fonction renvoie False en cas de non existance de cette donnée
+*/
 echo $db->getDataFor(array(
-    'key'   => 'user',
-    'value' => 'florian',
-    'data'  => 'test'
+    'key'   => 'user', // key est la façon dont cet utilisateur est identifié au sein de la base
+    'value' => 'flo', // value est son identifiant au sein de la base
+    'data'  => 'test' // data est le nom de la donnée
 ));
+$db->statsDb();
+$db->version();
